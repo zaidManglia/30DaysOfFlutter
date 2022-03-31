b707331fdea6ded1981f835662e22201e40c4001
zaid <zaidmanglia@gmail.com>
2022-03-31 22:43:30 +0530

    Day 4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,11 +4,43 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-            style: TextStyle(
-                fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold)),
-      ),
-    );
+        child: Column(
+      children: [
+        Image.asset("assets/images/photo.png"),
+        // SizedBox(height: 20.0, child: Text("Free space")),
+        Text(
+          "Welcome",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // SizedBox(height: 20.0, child: Text("Free space")),
+        Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password", labelText: "Password"),
+                ),
+                SizedBox(height: 20.0, child: Text("Free space")),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("Hi Codepur");
+                  },
+                )
+              ],
+            ))
+      ],
+    ));
   }
 }
